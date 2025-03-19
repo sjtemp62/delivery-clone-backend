3f600f49ecc8edbbd8ece640fa5f34a4c281b0bd
sjtemp62 <sjtemp62@gmail.com>
2025-03-19 18:02:09 +0900

    init: setup Spring Boot project

---
+++ src/main/java/com/clone/delivery/delivery_clone_backend/DeliveryCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.clone.delivery.delivery_clone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryCloneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clone/delivery/delivery_clone_backend/DeliveryCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.delivery.delivery_clone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
